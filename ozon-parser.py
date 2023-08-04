0f5e8914b1a8afd7239068ddb21f10e8f1b8f914
BaLaMyt900 <57708651+BaLaMyt900@users.noreply.github.com>
2023-08-04 10:32:23 +0400

    2 функции. Входной url со списком Входной url 1го товара

--- ozon-parser.py
+++ ozon-parser.py
@@ -27,62 +27,56 @@ class Selenium:
         self.driver.close()
         self.driver.quit()
 
-    def _parse_seller(self, url: str) -> str:
-        """ Парсинг страницы продавца. """
+    def _parse_page(self, url: str) -> BeautifulSoup:
+        """ Парсинг страницы """
         try:
             self._init_driver()
             self.driver.get(url)
             time.sleep(3)
             self.driver.execute_script('window.scrollTo(5, 4000);')
             time.sleep(5)
-            return self.driver.page_source
+            return BeautifulSoup(self.driver.page_source, features='html.parser')
         except Exception as ex:
             print(ex)
             self._delete_driver()
         finally:
             self._delete_driver()
 
-    def _parse_product(self, url: str) -> (str, int, dict):
+    def _parse_product(self, url) -> (str, int, dict):
         """ Сбор данных о продукте """
-        try:
-            self._init_driver()
-            self.driver.get(url)
-        except Exception as ex:
-            print(ex)
-            self._delete_driver()
+        if type(url) == str:
+            page = self._parse_page(url)
         else:
-            time.sleep(4)
-            self.driver.execute_script('window.scrollTo(5, 4000);')
-            time.sleep(3)
-            page = BeautifulSoup(self.driver.page_source, features='html.parser')
-            name = page.find(attrs={'data-widget': 'webProductHeading'}).find('h1').text
-            span_list = page.find_all('span')
-            prices_list = []
-            for item in span_list:
-                if '₽' in item.text:
-                    prices_list.append(item.text)
-            price = strToInt(prices_list[2])
-            div_characteristics = page.find('div', attrs={'data-widget': 'webCharacteristics'})
-            characteristics_name = div_characteristics.findAll('dt')
-            characteristics_value = div_characteristics.findAll('dd')
-            characteristics = {key.text: value.text for key, value in zip(characteristics_name, characteristics_value)}
-            return name, price, characteristics
-        finally:
-            self._delete_driver()
+            page = url
+        name = page.find(attrs={'data-widget': 'webProductHeading'}).find('h1').text
+        span_list = page.find_all('span')
+        prices_list = []
+        for item in span_list:
+            if '₽' in item.text:
+                prices_list.append(item.text)
+        price = strToInt(prices_list[2])
+        div_characteristics = page.find('div', attrs={'data-widget': 'webCharacteristics'})
+        characteristics_name = div_characteristics.findAll('dt')
+        characteristics_value = div_characteristics.findAll('dd')
+        characteristics = {key.text: value.text for key, value in zip(characteristics_name, characteristics_value)}
+        return name, price, characteristics
 
-    def parse(self, url: str, count: int):
-        """ Главная функция парсинга """
-        seller_page = self._parse_seller(url)
-        seller_page = BeautifulSoup(seller_page, features='html.parser')
-        href_list = seller_page.find(attrs={'data-widget': 'searchResultsV2'}).find_all('a')
-        product_list = []
-        for item in href_list:
-            link = f'https://www.ozon.ru{item.attrs["href"]}'
-            if link not in product_list:
-                product_list.append(link)
-        seller_name = seller_page.find('h1', class_='tsHeadline600Medium').string
-        seller = Seller.objects.create(name=seller_name, url=url)
-        for i in range(count):
-            name, price, characteristics = self._parse_product(product_list[i])
-            Product.objects.create(name=name, url=product_list[i], price=price, seller=seller,
-                                   characteristics=characteristics)
+    def parse(self, url: str):
+        """ Парсинг страницы. """
+        page = self._parse_page(url)
+        if page.find('div', attrs={'data-widget': 'webCharacteristics'}):  # Дана страница товара
+            name, price, characteristics = self._parse_product(page)
+            print(f'Название: {name}\nЦена:{price}\nХарактериски:{characteristics}\n')
+        elif page.find(attrs={'data-widget': 'searchResultsSort'}):  # Дан результат фильтра
+            href_list = page.find(attrs={'data-widget': 'searchResultsV2'}).find_all('a')
+            product_list = []
+            for item in href_list:
+                link = f'https://www.ozon.ru{item.attrs["href"]}'
+                if link not in product_list:
+                    product_list.append(link)
+            """ Вывод в консоль """
+            for i, item in enumerate(product_list):
+                name, price, characteristics = self._parse_product(item)
+                print(f'{i}) Название: {name}\nЦена:{price}\nХарактериски:{characteristics}\n')
+
+
